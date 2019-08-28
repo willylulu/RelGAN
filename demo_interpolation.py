4f3ed3dcf753d1c6d57731c5ec079df403ae67e0
willylulu <maya6282@gmail.com>
2019-08-28 22:47:45 +0800

    license

--- demo_interpolation.py
+++ demo_interpolation.py
@@ -1,3 +1,10 @@
+# Copyright (C) 2019 Willy Po-Wei Wu & Elvis Yu-Jing Lin <maya6282@gmail.com, elvisyjlin@gmail.com>
+# 
+# This work is licensed under the Creative Commons Attribution-NonCommercial
+# 4.0 International License. To view a copy of this license, visit
+# http://creativecommons.org/licenses/by-nc/4.0/ or send a letter to
+# Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+
 import os
 import sys
 import random
